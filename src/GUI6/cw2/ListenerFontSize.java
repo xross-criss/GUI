52eb063f65286588eaf4105af6b8f4a5d93df57e
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2017-06-05 11:38:15 +0200

    GUI 6 - exc.1 working. Test and small changes are required

---
+++ src/GUI6/cw2/ListenerFontSize.java
@@ -0,0 +1,29 @@
+package GUI6.cw2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ListenerFontSize implements ActionListener {
+
+    protected JTextArea handler;
+    protected int fontSize;
+
+    /**
+     * @param handler
+     * @param size
+     */
+    public ListenerFontSize(JTextArea handler, int size) {
+        this.handler = handler;
+        this.fontSize = size;
+    }
+
+    /**
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        handler.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+    }
+}
