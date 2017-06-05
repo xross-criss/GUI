302fb8049eb11ef9cc796b57f543ee577baa1243
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2017-06-05 11:42:26 +0200

    GUI 6 - excercise 1&2 - Test and small changes are required.

---
+++ src/GUI6/cw2/ListenerBackground.java
@@ -0,0 +1,27 @@
+package GUI6.cw2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ListenerBackground implements ActionListener {
+
+    protected JTextArea handler;
+
+    /**
+     * @param handler
+     */
+    public ListenerBackground(JTextArea handler) {
+        this.handler = handler;
+    }
+
+    /**
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JMenuItem item = (JMenuItem) e.getSource();
+        MyIcon icon = (MyIcon) item.getIcon();
+        handler.setBackground(icon.getColor());
+    }
+}
---
+++ src/GUI6/cw2/MyIcon.java
@@ -0,0 +1,62 @@
+package GUI6.cw2;
+
+import java.awt.*;
+
+public class MyIcon implements javax.swing.Icon {
+
+    protected Color color;
+    protected int widht, height;
+
+    /**
+     *
+     */
+    public MyIcon() {
+        this(10, 10, Color.GREEN);
+    }
+
+    /**
+     * @param color
+     */
+    public MyIcon(Color color) {
+        this(10, 10, color);
+    }
+
+    /**
+     * @param width
+     * @param height
+     */
+    public MyIcon(int width, int height) {
+        this(width, height, Color.GREEN);
+    }
+
+    /**
+     * @param width
+     * @param height
+     * @param color
+     */
+    public MyIcon(int width, int height, Color color) {
+        this.widht = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public int getIconHeight() {
+        return height;
+    }
+
+    @Override
+    public int getIconWidth() {
+        return widht;
+    }
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        g.setColor(color);
+        g.fillOval(x, y, widht, height);
+    }
+}
