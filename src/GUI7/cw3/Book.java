859ef103ab3475b99ec613aaa65d1846f492f60e
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2017-06-23 20:41:08 +0200

    working gui

--- src/GUI7/cw3/Book.java
+++ src/GUI7/cw3/Book.java
@@ -1,8 +1,10 @@
 package GUI7.cw3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Book {
 
@@ -12,53 +14,6 @@ public class Book {
     private String bookDescription;
     private String pictureURL;
 
-
-/*    */
-
-    /**
-     * Konstruktor tworzący
-     *
-     * @param author autor
-     * @param title  tutył książki
-     * @param price  cena książki
-     *//*
-    public Book(String author, String title, double price) {
-        super();
-        this.author = author;
-        this.title = title;
-        this.price = price;
-    }
-
-    public Book() {
-        super();
-        this.author = "";
-        this.title = "";
-        this.price = 0;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }*/
     public Book(String author, String title, double price, String bookDescription, String pictureURL) {
         this.author = author;
         this.title = title;
@@ -68,12 +23,7 @@ public class Book {
     }
 
     public Book() {
-        super();
-        this.author = "";
-        this.title = "";
-        this.price = 0;
-        this.bookDescription = "";
-        this.pictureURL = "";
+        this("", "", 0.0, "", "");
     }
 
     public String getAuthor() {
@@ -116,30 +66,36 @@ public class Book {
         this.pictureURL = pictureURL;
     }
 
-    public static String formatDescription(String bookDescription, int maxlimit) {
-        List<String> bookDescList = new ArrayList<>(Arrays.asList(bookDescription.split(" ")));
-        List<String> cp = new ArrayList<>();
-        StringBuilder sb;
-        int counter = 0;
-
-        for (int i = 0; i < bookDescList.size(); i++) {
-            if (counter == maxlimit) {
-                sb = new StringBuilder(bookDescList.get(i));
-                sb.append("/\n");
-                cp.add(sb.toString());
-                counter = 0;
-            } else {
-                cp.add(bookDescList.get(i));
-                counter++;
-            }
+    public ImageIcon getImageIcon() {
+        try {
+            final BufferedImage image = ImageIO.read(new File(getPictureURL()));
+            return new ImageIcon(resize(image, (int) Math.round(1.0 * image.getWidth() / (image.getWidth() / 64)), 64));
+        } catch (Exception e) {
+            return null;
         }
-
-        sb = new StringBuilder();
-
-        for (String s : cp) {
-            sb.append(s);
-        }
-
-        return sb.toString();
     }
+
+    private BufferedImage resize(BufferedImage originalImage, int width, int height) {
+        BufferedImage resizedImage = new BufferedImage(width, height, originalImage.getType());
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, width, height, null);
+        g.dispose();
+        g.setComposite(AlphaComposite.Src);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        return resizedImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", bookDescription='" + bookDescription + '\'' +
+                ", pictureURL='" + pictureURL + '\'' +
+                '}';
+    }
+
 }
--- src/GUI7/cw3/Main.java
+++ src/GUI7/cw3/Main.java
@@ -1,17 +1,10 @@
 package GUI7.cw3;
 
-import javax.swing.JFrame;
-
-
 public class Main {
     public static void main(String... args) {
 
-        JTableDemo mainFrame = new JTableDemo();
-
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.pack();
-        mainFrame.setLocationByPlatform(true);
-        mainFrame.setVisible(true);
+        MyFrame frame = new MyFrame();
+        frame.setVisible(true);
 
     }
 }
